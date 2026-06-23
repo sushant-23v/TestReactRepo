2dc8b035a19d43e1f8989549f4e7d80f3a1a92f3
sushant-23v <sushant@23v.co>
2026-06-23 10:51:32 +0530

    about-updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,14 +2,14 @@ export default function About() {
   return (
     <section id="about" className="about">
       <div className="container">
-        <h2>About Us</h2>
+        <h2>About </h2>
         <p>
-          At Brew & Bean, we believe great coffee brings people together. Our baristas
-          roast and brew with care, highlighting nuanced flavors from sustainably
-          sourced beans. Settle into a warm atmosphere—perfect for catch-ups, remote
-          work, or a quiet moment to yourself.
+          At Brew & Bean, we believe great coffee brings people together. Our
+          baristas roast and brew with care, highlighting nuanced flavors from
+          sustainably sourced beans. Settle into a warm atmosphere—perfect for
+          catch-ups, remote work, or a quiet moment to yourself.
         </p>
       </div>
     </section>
-  )
+  );
 }
